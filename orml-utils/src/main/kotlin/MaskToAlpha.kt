45a6938f9520c02a766badb666569b690a52adc8
Edwin Jakobs <edwin@rndr.studio>
2020-11-15 09:20:31 +0100

    [orml-utils] tweak MaskToAlpha tresholding settings

--- orml-utils/src/main/kotlin/MaskToAlpha.kt
+++ orml-utils/src/main/kotlin/MaskToAlpha.kt
@@ -12,17 +12,34 @@ in vec2 v_texCoord0;
 uniform sampler2D tex0;
 uniform sampler2D tex1;
 uniform bool invert;
+uniform bool useThreshold;
+uniform bool useSmoothstep;
+uniform float threshold;
+uniform float low;
+uniform float high;
 out vec4 o_output;
              
 void main() {
     float mask = texture(tex1, v_texCoord0).r;
+    mask = useSmoothstep? smoothstep(low, high, mask) : mask;
+    mask = useThreshold? step(threshold, mask) : mask;
     mask = invert? (1.0 - mask) : mask;
     o_output = texture(tex0, v_texCoord0) * mask;
 }             
 """, "mask-to-alpha")) {
 
     var invert: Boolean by parameters
+    var useThreshold : Boolean by parameters
+    var useSmoothstep : Boolean by parameters
+    var threshold : Double by parameters
+    var low: Double by parameters
+    var high: Double by parameters
     init {
         invert = false
+        useThreshold = false
+        useSmoothstep = true
+        threshold = 0.9
+        low = 0.49
+        high = 0.5
     }
 }
