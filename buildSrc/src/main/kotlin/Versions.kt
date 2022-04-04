df63c1758ac2c8f7a343d8455edb8d0ba54b4467
Rein van der Woerd <reinvanderwoerd@me.com>
2022-04-04 16:53:09 +0200

    Upgrade to Gradle 7.4.2, Kotlin 1.6.20

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,5 +3,5 @@ object Versions {
     var openrndrUseSnapshot = true
     var orx = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.0-rc.8"
     var openrndr = if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.0-rc.7"
-    val kotlin = "1.6.10"
+    val kotlin = "1.6.20"
 }
